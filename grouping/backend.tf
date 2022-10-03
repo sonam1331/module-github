951dddbe91b26500f5458b23e0656aa28a9d31f7
sonam1331 <sonamsandeep1331@gmail.com>
2022-10-03 21:49:18 +0530

    some addition

--- grouping/backend.tf
+++ grouping/backend.tf
@@ -1,15 +1,15 @@
-terraform {
-  backend "s3" {
-#<<<<<<< HEAD
-    # bucket = "sonam-bucket-tf"
-    # key    = "new.tfstate"
-    # region = "us-east-1"
-    # dynamodb="sonam-db"
-#=======
-    # bucket = "sonam-bucket-tf"
-    # key    = "new.tfstate"
-    # region = "us-east-1"
-    # #dynamodb="sonam-db"
-#>>>>>>> 7bc95e28f869b4941599f61aad25a13b7650717c
-  }
-}
+# terraform {
+#   backend "s3" {
+# #<<<<<<< HEAD
+#     # bucket = "sonam-bucket-tf"
+#     # key    = "new.tfstate"
+#     # region = "us-east-1"
+#     # dynamodb="sonam-db"
+# #=======
+#     # bucket = "sonam-bucket-tf"
+#     # key    = "new.tfstate"
+#     # region = "us-east-1"
+#     # #dynamodb="sonam-db"
+# #>>>>>>> 7bc95e28f869b4941599f61aad25a13b7650717c
+#   }
+# }
