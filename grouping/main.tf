d3b9e6833c54b9e6ab996cefdc15a8d7442083f2
sonam1331 <sonamsandeep1331@gmail.com>
2022-10-03 17:08:43 +0530

    some changes

--- grouping/main.tf
+++ grouping/main.tf
@@ -4,7 +4,7 @@ module "iam-user"{
 }
 module "iam-group"{
     source = "../iamgrp"
-<<<<<<< HEAD
+#<<<<<<< HEAD
     groupname = "sonam-practicing"
 }
 module "iam-membership"{
@@ -13,7 +13,7 @@ module "iam-membership"{
     username = [for x,y in var.username1:y]
     groupname = "sonam-practicing"
 }
-=======
-    groupname = "ptractise"
-}
->>>>>>> 7bc95e28f869b4941599f61aad25a13b7650717c
+#=======
+    #groupname = "ptractise"
+
+#>>>>>>> 7bc95e28f869b4941599f61aad25a13b7650717c
